71b9f14236f052dd9cb1b76a404aa370b605979e
Aiden <aferg6@uwo.ca>
2024-11-26 19:49:15 -0500

    implamented filter bar

--- JobAnalysis.js
+++ JobAnalysis.js
@@ -1,13 +1,36 @@
+//definining document elements
 let fileInput = document.getElementById("fileInput");
-let fileStatusText = document.getElementById("fileStatus")
+let fileStatusText = document.getElementById("fileStatus");
+let filterMenu = document.getElementById("filterMenu");
 
-fileInput.addEventListener('change', function(){
-    var file = target.file;
-    var extension = target.extension;
 
-    if(file != null){
-        fileStatusText.textContent("file loaded");
+// class job {
+//     constructor
+// }
+
+
+//event listener to change the ui when a json file is loaded
+fileInput.addEventListener('change', function(event){
+    var userFile = event.target.files[0];
+    var fileParts= userFile.name.split(".");
+    var fileExtension = fileParts[1];
+    
+    //varifies json file and prompts user to input correct filetype otherwise
+    if(fileExtension != "json"){
+        fileStatusText.textContent = "Incorrect file type please select a json file";
+        fileStatusText.style.display = "block";
+        filterMenu.style.display = "block";
     }else{
-        fileStatusText.textContent("no file loaded");
+        fileStatusText.textContent = "";
+        fileStatusText.style.display = "none";
+        filterMenu.style.display = "flex";
+
+
+
+        const reader = new FileReader();
+
+
+
     }
+    
 })
